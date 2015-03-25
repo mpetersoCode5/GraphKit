d0ad31480d5627bbd841ce5648eccee2be23dfda
Michael Peterson <michaelpeterson@Michaels-iMac.local>
2015-03-25 18:57:47 -0400

    Initial Commit

---
+++ SwiftGraph/BarLayer.swift
@@ -0,0 +1,16 @@
+//
+//  BarLayer.swift
+//  SwiftGraph
+//
+//  Created by Michael Peterson on 3/25/15.
+//  Copyright (c) 2015 Michael Peterson. All rights reserved.
+//
+
+import UIKit
+
+public class BarLayer: CALayer {
+    public func CreateLayer(height : CGFloat, color : CGColor, location : CGPoint)
+    {
+        
+    }
+}
---
+++ SwiftGraph/SwiftGraph.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGraph.h
+//  SwiftGraph
+//
+//  Created by Michael Peterson on 3/25/15.
+//  Copyright (c) 2015 Michael Peterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftGraph.
+FOUNDATION_EXPORT double SwiftGraphVersionNumber;
+
+//! Project version string for SwiftGraph.
+FOUNDATION_EXPORT const unsigned char SwiftGraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGraph/PublicHeader.h>
+
+
